e9ec181bc981f17bee0f7dcc4d9686d61ef8d822
Ercolani <mario.ercolani@accenture.com>
2023-07-14 15:59:55 -0300

    Modulo 4: Final

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,9 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover:'https://www.lanacion.com.ar/resizer/ykH6qXqi7iWs3b9M_uBYqwKvu_0=/880x586/smart/filters:format(webp):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/AGUGPJKPNZEQHIF4TYLVFAJLFU.jpg',
     album:'Gioli y ashia',
-    name:'BEBE (Oficial)'
+    name:'BEBE (Oficial)',
+    url: 'mp3',
+    _id: 1
   }
 }
