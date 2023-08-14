b28690ebdd280381f6beacd903f0c2a3639b26d8
Ercolani <mario.ercolani@accenture.com>
2023-08-14 13:20:45 -0300

    Testing y

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPageComponent } from './login-page.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -20,6 +21,65 @@ describe('LoginPageComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+
+  //TODO: Tu primer enunciado el cual debe de asegurar lo siguiente
+  //TODO: Debe de asegurarse que el formulario sea invalido cuando ingrese dato erroneos
+
+  //TODO: Patron AAA
+
+  it('🔴 Deberia de retornar "invalido" el formulario', () => {
+
+    //TODO: Arrange
+    const mockCredentials = {
+      email: '0x0x0x0x0x0',
+      password: '1111111111111111111111111'
+    }
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    //TODO: Act
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    //TODO: Assert
+
+    expect(component.formLogin.invalid).toEqual(true);
+  });
+
+  it('✔✔ Deberia de retornar "valido" el formulario', () => {
+
+    //TODO: Arrange
+    const mockCredentials = {
+      email: 'test@test.com',
+      password: '12345678'
+    }
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    //TODO: Act
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    //TODO: Assert
+
+    expect(component.formLogin.invalid).toEqual(false);
+  });
+
+  it('👍 El boton deberia de tener la palabra "Iniciar sesión" y lo tiene', () => {
+
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'))
+    const getInnerText = elementRef.nativeElement.innerText
+
+    expect(getInnerText).toEqual('Iniciar sesión')
+
+  })
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
